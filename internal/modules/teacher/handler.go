51a5cc746dbb3b572a7624483016ccc096af7556
TheGauravsahu <itsgauravsahu7@gmail.com>
2025-11-26 13:16:55 +0530

    feat : add create teacher

--- internal/modules/teacher/handler.go
+++ internal/modules/teacher/handler.go
@@ -1,11 +1,68 @@
 package teacher
 
+import (
+	"net/http"
+
+	"github.com/TheGauravsahu/school-api/internal/modules/user"
+	"github.com/TheGauravsahu/school-api/internal/utils"
+)
+
 type Handler struct {
 	TeacherRepo *Repository
+	UserRepos   *user.Repository
 }
 
-func NewHandler(teacherRepo *Repository) *Handler {
+func NewHandler(teacherRepo *Repository, userRepo *user.Repository) *Handler {
 	return &Handler{
 		TeacherRepo: teacherRepo,
+		UserRepos:   userRepo,
 	}
+}
+
+type CreateTeacherRequest struct {
+	FirstName string `josn:"first_name"`
+	LastName  string `josn:"last_name"`
+	Email     string `json:"email"`
+	Subject   string `josn:"subject"`
+	PhoneNo   string `josn:"phone"`
+	SchoolID  uint   `json:"school_id"`
+	ClassID   uint   `json:"class_id"`
+}
+
+func (h *Handler) CreateTeacher(w http.ResponseWriter, r *http.Request) {
+	var body CreateTeacherRequest
+	if ok := utils.ParseJson(w, r, &body); !ok {
+		return
+	}
+
+	username := utils.GenerateUsername(body.FirstName, body.LastName, int(body.SchoolID))
+	password := utils.GeneratePassword()
+	hashed, _ := utils.HashPassword(password)
+
+	user := &user.User{
+		Username: username,
+		Password: hashed,
+		Role:     "TEACHER",
+		SchoolID: body.SchoolID,
+	}
+	h.UserRepos.CreateUser(user)
+
+	teacher := &Teacher{
+		FirstName: body.FirstName,
+		LastName:  body.LastName,
+		PhoneNo:   body.PhoneNo,
+		Subject:   body.Subject,
+		SchoolID:  body.SchoolID,
+		UserID:    user.ID,
+	}
+	h.TeacherRepo.CreateTeacher(teacher)
+
+	if teacher.Email != "" {
+		utils.SendWelcomeEmail(teacher.Email, username, password)
+	}
+
+	utils.WriteJson(w, http.StatusCreated, map[string]interface{}{
+		"message": "Teacher created sucessfully.",
+		"teacher": teacher,
+	})
 }
--- internal/modules/teacher/model.go
+++ internal/modules/teacher/model.go
@@ -1,10 +1,18 @@
 package teacher
 
+import "time"
+
 type Teacher struct {
 	ID        uint   `gorm:"primaryKey"`
 	UserID    uint   `gorm:"not null;unique"`
+	SchoolID  uint   `gorm:"not null"`
+	ClassID   uint   `gorm:"not null"`
+	Email     string `gorm:"not null"`
 	FirstName string `gorm:"not null"`
 	LastName  string `gorm:"not null"`
 	Subject   string `gorm:"not null"`
 	PhoneNo   string `gorm:"not null"`
+
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
